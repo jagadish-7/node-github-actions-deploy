fc0e2ecace889633b6fdbdf429182410f9e4ca85
jagadish-7 <coderjagadish143@gmail.com>
2026-06-28 12:32:02 +0530

    test: github actions ci/cd

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = process.env.PORT ?? 8080;
 
 app.get('/', (req, res) => {
-    return res.json({msg: "Hello from the express server"})
+    return res.json({msg: "Hello from the express server. The CI/CD is working using github actions"})
 })
 
 app.listen(PORT, () => {
